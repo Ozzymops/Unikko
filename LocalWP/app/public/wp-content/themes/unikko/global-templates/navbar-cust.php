71c1cd29e489e014b3ade98ae9c097d624ad676a
Ozzymops <justinmuris@gmail.com>
2026-06-17 17:38:41 +0200

    Fixed theme irregularities

--- LocalWP/app/public/wp-content/themes/unikko/global-templates/navbar-cust.php
+++ LocalWP/app/public/wp-content/themes/unikko/global-templates/navbar-cust.php
@@ -29,7 +29,7 @@ $container = get_theme_mod( 'understrap_container_type' );
         </div>
 
         <div class="nav-subtitle">
-            <p class="nav-subtitle">Praktijk voor individuele begeleiding en coachende & verdiepende gesprekken</p>
+            <p class="nav-subtitle">Praktijk voor individuele begeleiding & verdiepende gesprekken</p>
         </div>
 		
         <!-- Bottom row: menu -->
@@ -37,12 +37,12 @@ $container = get_theme_mod( 'understrap_container_type' );
             <button class="nav-toggle">☰ Menu</button>
 
             <ul class="nav-menu">
-                <li><a href="<?php echo home_url('/home'); ?>"><img class="nav-menu-image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/menu-home.png" /></a></li>
-                <li><a href="<?php echo home_url('/over-ons'); ?>"><img class="nav-menu-image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/menu-overons.png" /></a></li>
-                <li><a href="<?php echo home_url('/verwijzers'); ?>"><img class="nav-menu-image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/menu-verwijzers.png" /></a></li>
-                <li><a href="<?php echo home_url('/praktisch'); ?>"><img class="nav-menu-image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/menu-praktisch.png" /></a></li>
-                <li><a href="<?php echo home_url('/agenda'); ?>"><img class="nav-menu-image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/menu-agenda.png" /></a></li>
-                <li><a href="<?php echo home_url('/contact'); ?>"><img class="nav-menu-image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/menu-contact.png" /></a></li>
+                <li><a href="<?php echo home_url('/index.php/home'); ?>"><img class="nav-menu-image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/menu-home.png" /></a></li>
+                <li><a href="<?php echo home_url('/index.php/over-ons'); ?>"><img class="nav-menu-image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/menu-overons.png" /></a></li>
+                <li><a href="<?php echo home_url('/index.php/verwijzers'); ?>"><img class="nav-menu-image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/menu-verwijzers.png" /></a></li>
+                <li><a href="<?php echo home_url('/index.php/praktisch'); ?>"><img class="nav-menu-image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/menu-praktisch.png" /></a></li>
+                <li><a href="<?php echo home_url('/index.php/agenda'); ?>"><img class="nav-menu-image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/menu-agenda.png" /></a></li>
+                <li><a href="<?php echo home_url('/index.php/contact'); ?>"><img class="nav-menu-image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/menu-contact.png" /></a></li>
             </ul>
         </div>
 
